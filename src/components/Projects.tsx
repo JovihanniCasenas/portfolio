dfb4d5c163bdcfad295d8f789c246115bccc0a1e
Jovihanni Casenas <60131810+JovihanniCasenas@users.noreply.github.com>
2025-07-08 01:51:30 -0400

    feat: add hulagway to projects (#9)

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,14 +3,15 @@ import colors from "../colors"
 
 const Projects = () => {
     return (
-        <div className="page-wrapper">
+        <div className="page-wrapper" style={{ marginBottom: "60px"}}>
             <h1 id="projects">Projects</h1>
-            <Grid2 container>
+            <Grid2 justifyContent={"center"} container>
                 <Grid2
-                    size={{ xs: 12, sm: 12, md: 6, lg: 6, xl: 6 }}
+                    size={{ xs: 12, sm: 12, md: 4, lg: 3, xl: 3 }}
                     justifyContent={"center"}
                     display={"flex"}
                     padding={"20px"}
+                    margin={"20px"}
                 >
                     <a
                         href="https://wondererxwanderer.netlify.app"
@@ -30,10 +31,11 @@ const Projects = () => {
                 </Grid2>
 
                 <Grid2
-                    size={{ xs: 12, sm: 12, md: 6, lg: 6, xl: 6 }}
+                    size={{ xs: 12, sm: 12, md: 4, lg: 3, xl: 3 }}
                     justifyContent={"center"}
                     display={"flex"}
                     padding={"20px"}
+                    margin={"20px"}
                 >
                     <a
                         href="https://jovihannicasenas.github.io/high-low-game-web-app/#/"
@@ -45,6 +47,30 @@ const Projects = () => {
                             <p className="project-card-title text-lg" style={{ color: colors.tertiary }}>High Low</p>
                             <p className="project-card-content text-sm" style={{ color: colors.tertiary }}>
                                 Interactive game developed using flutter. Can be played via mobile, desktop, or web.
+                            </p>
+                        </div>
+                    </a>
+                </Grid2>
+
+                <Grid2
+                    size={{ xs: 12, sm: 12, md: 4, lg: 3, xl: 3 }}
+                    justifyContent={"center"}
+                    display={"flex"}
+                    padding={"20px"}
+                    margin={"20px"}
+                >
+                    <a
+                        href="https://github.com/JovihanniCasenas/hulagway"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <div className="project-card">
+                            <Avatar alt="Hulagway" src="/assets/hulagway.png" sx={{ width: 100, height: 100 }} />
+                            <p className="project-card-title text-lg" style={{ color: colors.tertiary }}>Hulagway</p>
+                            <p className="project-card-content text-sm" style={{ color: colors.tertiary }}>
+                                Image gallery to manage local storage images meant to be hosted locally along with
+                                tools like Tailscale for outside the network access.
+                                Created using ReactJS, Next.js, and Tailwind CSS.
                             </p>
                         </div>
                     </a>

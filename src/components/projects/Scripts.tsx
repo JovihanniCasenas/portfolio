be9ac1705ee2865b98e7d6a988546a81b5deaf63
JovihanniCasenas <casenasjovihanni@gmail.com>
2025-10-16 18:40:56 +0800

    feat: improve projects ui

--- src/components/projects/Scripts.tsx
+++ src/components/projects/Scripts.tsx
@@ -5,7 +5,7 @@ const Scripts = () => {
     return (
         <Grid2 container spacing={2} style={{ marginBottom: "20px" }}>
             <Grid2 size={{ xs: 12, sm: 12, md: 6 }}>
-                <img src="/assets/sheets-script.png" alt="Google Sheets Script" style={{ maxWidth: "100%", height: "auto" }} />
+                <img src="/assets/sheets-script.png" alt="Google Sheets Script" style={{ maxWidth: "100%", height: "auto", borderRadius: '8px' }} />
             </Grid2>
             <TextWrapper>
                 Integrated the above Google Sheets script to automate counting of cells of specific colors

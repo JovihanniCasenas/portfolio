35b022ac1a7f44bc6bc3bea909b62628f59322fd
Jovihanni Casenas <60131810+JovihanniCasenas@users.noreply.github.com>
2025-11-30 23:45:26 +0800

    feat: use next third parties for gtm (#25)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next"
-import Script from 'next/script'
+import { GoogleTagManager } from '@next/third-parties/google'
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 import colors from "@/utils/colors"
@@ -29,27 +29,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <Script id="gtm-head" strategy="afterInteractive">
-          {`
-            function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-N2TGBGJ8');
-          `}
-        </Script>
-      </head>
+      <GoogleTagManager gtmId="GTM-N2TGBGJ8" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* <!-- Google Tag Manager (noscript) --> */}
-        <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N2TGBGJ8"
-            height="0" width="0" style={{display:'none',visibility:'hidden'}}>
-          </iframe>
-        </noscript>
-        {/* <!-- End Google Tag Manager (noscript) --> */}
         <div className="animated-background" style={{
           background: `linear-gradient(125deg, 
           ${colors.skyGradientStart} 0%, 

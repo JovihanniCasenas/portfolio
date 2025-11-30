4246f79ef4514bf5f75a138466b2fc4234a090a5
JovihanniCasenas <casenasjovihanni@gmail.com>
2025-11-30 23:07:25 +0800

    feat: add timeline component for work experience page

--- src/components/pages/WorkExperience.tsx
+++ src/components/pages/WorkExperience.tsx
@@ -1,39 +1,17 @@
 "use client"
-import colors from "@/utils/colors"
+
+import ExperienceTimeline from "../experience/ExperienceTimeline"
+import CustomButton from "../CustomButton"
 
 const WorkExperience = () => {
-    const Alphaus = () => (
-        <a
-            href="https://alphaus.cloud"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            <p
-            className="text-md"
-            style={{
-                color: colors.textLight,
-            }}
-            >Alphaus Inc.</p>
-        </a>
-    )
     return (
         <div className="page-wrapper">
             <h1 id="work">Work Experience</h1>
-            <p className="text-lg">Dev Team Lead</p>
-            <Alphaus />
-            <p className="text-md">October 2024-present</p>
-            <div
-            style={{
-                height: "100px",
-                width: "100px",
-                backgroundImage: "url('/assets/rocket.gif')",
-                backgroundSize: "cover",
-                margin: "10px",
-            }}
+            <ExperienceTimeline />
+            <CustomButton
+                text="View My Resume"
+                onClick={() => {window.open('https://drive.google.com/file/d/1J4pcn-YbDk2HcPeVMQ_3pUY485Du3AKR/view?usp=sharing')}}
             />
-            <p className="text-lg">Junior Software Engineer</p>
-            <Alphaus />
-            <p className="text-md">August 2023-September 2024</p>
         </div>
     )
 }
